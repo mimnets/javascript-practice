fda0d3289e5e51bdee4334306963a5d2670ac290
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-09-13 19:12:17 +0600

    less thab greater than

--- operators.js
+++ operators.js
@@ -9,7 +9,18 @@
 ++	Increment
 --	Decrement
 */
+var apple = 200;
+var mango = 130;
+var fruits = 0;
 
+fruits = apple + mango;
+var tax = 2; // 2 percent
+
+var fruitsTax = (fruits * 2) / 100;
+
+var fruitsPrice = fruits + fruitsTax;
+
+console.log(fruitsPrice);
 //different types of assignment operators
 /*
 =
@@ -22,3 +33,25 @@
 %=
 */
 
+//apple price increase with += and ++ operator
+apple += 30;
+
+apple++;
+
+// console.log(apple);
+
+// mango price decrease with -= and -- operator
+mango -= 30;
+mango--;
+
+var guava = 30;
+// console.log(mango);
+
+//comparison with operator
+
+if (mango > apple){
+    console.log('Buy apple');
+}
+else if (mango < apple){
+    console.log('Buy mango');
+}    
