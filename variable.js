b76abbfdb892359b047f2fe92150b947755244c7
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-09-13 11:35:28 +0600

    variable declation and change the value of the variable after declaration

---
+++ variable.js
@@ -0,0 +1,24 @@
+// JavaScript vairable declaration
+
+var firstName = 'monirul'; //this is variable and it is string type variable so it must be within a double or single quote
+var lastName = 'islam';
+
+var fullName = firstName + ' ' + lastName; // to add two string variables data we have to use + operator with single quote with free space.
+
+
+
+console.log(fullName);
+
+// If we console.log then the result will be : monirul islam
+
+// how to change the vaiable value
+
+firstName = 'Monirul';
+lastName = 'Islam';
+
+fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+//Look the two console.log results - first one with small case letter and second one with upper case letter.
+
