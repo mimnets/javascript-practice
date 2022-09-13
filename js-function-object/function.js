379294de5c48eb837720a5614f84589476b96059
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-09-13 23:17:08 +0600

    function with parameter

---
+++ js-function-object/function.js
@@ -0,0 +1,9 @@
+function  startFan(){
+    console.log('Stand up');
+    console.log('walk towards the switch');
+    console.log('press the switch');
+}
+
+// call the function
+
+startFan();
---
+++ js-function-object/parameter.js
@@ -0,0 +1,10 @@
+function bringSingara(money){
+    console.log('Mama singara den');
+    console.log(money);
+}
+
+//call the function
+
+bringSingara(123);
+
+
