48f9fcb84e108cd7d2e9e82c5c4600362c592a72
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-09-13 12:58:48 +0600

    get the items of the index

--- array.js
+++ array.js
@@ -1,8 +1,18 @@
 //array declaration
 var tableItems = ['mouse', 'pen', 'books', 'printer', 'bottle', 'speaker'];
 
+// array elements
+// console.log(tableItems.length);
 
 //array index
 var letIndex = tableItems.indexOf('mouse');
 
-console.log(letIndex);
+// console.log(letIndex);
+
+//change a array index items value
+
+tableItems[0] = 'watch';
+
+//get the items of the index
+ 
+console.log(tableItems[0]);
