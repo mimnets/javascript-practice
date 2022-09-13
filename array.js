eaa04cde694454965f3c6ee4d386351068d95e34
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-09-13 12:41:01 +0600

    get array index with tableIndex.indexOff('mouse')

---
+++ array.js
@@ -0,0 +1,8 @@
+//array declaration
+var tableItems = ['mouse', 'pen', 'books', 'printer', 'bottle', 'speaker'];
+
+
+//array index
+var letIndex = tableItems.indexOf('mouse');
+
+console.log(letIndex);
