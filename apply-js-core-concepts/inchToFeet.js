db818968d34987e69d0e35e5a97d4201544c3264
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-09-14 21:20:20 +0600

    inch to feet conversion

---
+++ apply-js-core-concepts/inchToFeet.js
@@ -0,0 +1,19 @@
+// const myInches = 12;
+// const myFeet = myInches / 12;
+
+// console.log(myFeet);
+
+function inchToFeet(inches){
+    const feet = inches / 12;
+    return feet;
+}
+
+const dadaInches = 60;
+const dadaFeet = inchToFeet(dadaInches);
+
+console.log(dadaFeet);
+
+const dadiInches = 50;
+const dadiFeet = inchToFeet(dadiInches);
+
+console.log(dadiFeet);
---
+++ apply-js-core-concepts/recap.js
@@ -0,0 +1,49 @@
+//1. variable
+var myName = 'Khan';
+
+//math operation: +, -, *, /
+//shorthand: +=, -=, *=, /=
+
+//++, --
+
+//2. array
+var friends =['abul', 'chabul', 'kabul', 'Ebul'];
+
+var thirdFriend = friends[2];
+friends[3] = 'Dambul';
+
+//3. conditionals
+//>, <, >=, <=, ==, ===, !=, !==
+
+if(friends.length < 2){
+    console.log('Fokira tor kono friend nai');
+}
+else{
+
+}
+
+//4. Loop
+var naumber = 0;
+while (number <= 6){
+    console.log(number);
+    number++;
+}
+
+for(var i = 0; i <= 10; i++){
+
+}
+
+//5. Function
+function isMoonUp(time){
+    if(time > 7){
+        return true;
+    }
+}
+
+//6. Object
+var jantus = {
+    height: 60,
+    romanticism: 'heavy',
+    bappErTaka: 'borolox'
+}
+
