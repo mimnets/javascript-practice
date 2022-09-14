4b3b074048d4948ee30506b10ab4c3a13a0aeb22
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-09-14 17:03:58 +0600

    object looping

--- js-function-object/object.js
+++ js-function-object/object.js
@@ -1,19 +1,26 @@
-var student = { 
-    id: 115, 
-    name: 'Solaiman Khan', 
-    class: 9, 
-    marks: 98
-};
+// var student = { 
+//     id: 115, 
+//     name: 'Solaiman Khan', 
+//     class: 9, 
+//     marks: 98
+// };
 
-var mobile = {
-    brand: 'Samsung',
-    price: 19000,
-    storage: 64,
-    camera: '7MP'
-};
+// var mobile = {
+//     brand: 'Samsung',
+//     price: 19000,
+//     storage: 64,
+//     camera: '7MP'
+// };
 
-var objectProperties = mobile.brand;
-console.log(mobile);
-mobile.camera = '12mp'; //this is how to change object properties value
-console.log(mobile);
-console.log(objectProperties);
+// var objectProperties = mobile.brand;
+// console.log(mobile);
+// mobile.camera = '12mp'; //this is how to change object properties value
+// console.log(mobile);
+// console.log(objectProperties);
+
+function movie(){
+    return "Din-The day";
+    return "Poran";
+    return "Hawa";
+  }
+  console.log(movie());
