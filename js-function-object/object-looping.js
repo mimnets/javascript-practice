04cd2185237b3d98011745b09f7195243adb3f2e
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-09-14 16:41:43 +0600

    object property, values with for in

--- js-function-object/object-looping.js
+++ js-function-object/object-looping.js
@@ -1,3 +1,12 @@
+var shoppingItems = ['books', 'sunglass', 'shows', 'pen'];
+var friendsAge = [12,23,44,5,66,77,45];
+var friendsAge = {
+    rahim: 12,
+    monir: 78,
+    abid: 23,
+    kazi: 23
+};
+
 var shoppingCart = {
     books: 3,
     sunglass: 1,
@@ -6,3 +15,21 @@ var shoppingCart = {
     pen: 25
 };
 
+const keys = Object.keys(shoppingCart);
+// console.log(keys);
+
+const values = Object.values(shoppingCart);
+
+// console.log(values);
+
+for(var i = 0; i < keys.length; i++){
+    var propertyName = keys[i];
+    var propertyValue = shoppingCart[propertyName];
+    // console.log(propertyName, propertyValue);
+}
+
+// for in loop
+for (var propertyName in shoppingCart) {
+    const value = shoppingCart[propertyName];
+    console.log(propertyName, value);
+};
