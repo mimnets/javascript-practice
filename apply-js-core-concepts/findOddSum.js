5ad2ec73ed5e9d80aed0932957ee86f621375bb6
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-09-15 21:04:25 +0600

    find even or odd numbers from an array

--- apply-js-core-concepts/findOddSum.js
+++ apply-js-core-concepts/findOddSum.js
@@ -17,11 +17,20 @@ function getSumOfAnArry(numbers){
         const index = i;
         const element = numbers[index];
         sum = sum + element;
-        console.log(index, element, sum);
+        // console.log(index, element, sum);
     }
     return sum;
 }
 
-const myNumbers = [12, 34, 25, 43, 89, 45, 62, 65, 77, 78, 84];
-
-getSumOfAnArry(myNumbers);
+//find odd or even numbers in an array
+function getOddNumbersOfAnArry(myNumbers) {
+    for(let i = 0; i < myNumbers.length; i++) {
+        const index = i;
+        const element = myNumbers[index];
+        if(element % 2 !== 0) {
+            console.log(index, element);
+}
+}
+}
+const numbers = [12, 34, 25, 43, 89, 45, 62, 65, 77, 78, 84];
+getOddNumbersOfAnArry(numbers)
