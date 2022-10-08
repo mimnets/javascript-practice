98a966b8b058cc157b87f4a9bab5b5f834b0ee53
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-10-08 18:30:45 +0600

    multiline arrow function and no parameter arrow function

--- module-30/arrow.js
+++ module-30/arrow.js
@@ -19,5 +19,16 @@ const add2 = function (first, second){
 // arrow function
 const add3 = (first, second) => first + second;
 
+// no parameter arrow function
+const getpie = () => 3.14;
+
+// Multi line arrow function
+const doMath = (x, y, z) => {
+    const firstSum = x + y;
+    const secondSum = y + z;
+    const multiplyResult = firstSum * secondSum;
+    const result = multiplyResult / 2;
+    return result;
+}
 const result = add(10, 20);
 console.log(result);
