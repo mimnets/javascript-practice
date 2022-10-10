f8ff55fc20c9682e240099501a79547cfd2f0d25
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-10-10 23:00:20 +0600

    fetch url and use this

---
+++ module-30/expre-api/js/app.js
@@ -0,0 +1,10 @@
+function loadUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => displayUsers(data))
+
+}
+
+function displayUsers(data){
+    console.log(data);
+}
